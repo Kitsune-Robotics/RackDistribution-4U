6264e602778ad9966a23a41c42002622ce060f9f
KenwoodFox <joe@kitsunehosting.net>
2026-09-18 11:18:45 -0400

    Tune fan curves

--- Firmware/lib/fans/profile/pump.h
+++ Firmware/lib/fans/profile/pump.h
@@ -5,8 +5,8 @@
 /* not really tuned, stays flat till it's hot */
 #define FAN_PROFILE_PUMP \
   FAN_CH(FAN_PUMP, 800,  \
-         {0.0f, 160},    \
-         {5.0f, 175},    \
-         {10.0f, 190},   \
-         {15.0f, 220},   \
-         {20.0f, 255})
+         {0.0f, 180},    \
+         {5.0f, 180},    \
+         {10.0f, 180},   \
+         {12.0f, 255},   \
+         {15.0f, 255})
