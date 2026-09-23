ebb82ccc891ef8f0bd4096504117986a33ae6da0
KenwoodFox <joe@kitsunehosting.net>
2026-09-23 10:29:11 -0400

    Tuning

--- Firmware/include/parameters.h
+++ Firmware/include/parameters.h
@@ -11,8 +11,8 @@
 #define A_LITTLE_HOT_ABOVE_AIR_C 6.0f
 #define A_LITTLE_HOT_HISTERESIS_C 0.25f
 #define COOLDOWN_COLD_ABOVE_AIR_C 6.0f
-#define TSENSOR0_OFFSET_C 1.9f /* coolant */
-#define TSENSOR1_OFFSET_C -1.9f /* air */
+#define TSENSOR0_OFFSET_C 1.5f /* coolant */
+#define TSENSOR1_OFFSET_C -1.5f /* air */
 #define TSENSOR2_OFFSET_C 0.0f /* exhaust */
 #define AMBIENT_FALLBACK_C 20.0f
 
